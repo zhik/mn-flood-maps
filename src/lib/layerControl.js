3ed7dc6244be92a1d531a4e862ae26f6944e73f5
zhik <zhikhe@gmail.com>
2022-08-17 16:04:49 -0400

    add subway layer

--- src/lib/layerControl.js
+++ src/lib/layerControl.js
@@ -13,15 +13,107 @@ import thumb_sandy_inundation from '../assets/sandy_inundation.png'
 import { csvParse } from 'd3-dsv'
 import proj4 from 'proj4'
 
+import subwayLines from './subwaylines.geo.json'
+import subwayStations from './subwaystations.geo.json'
+
 const wkid4326 = '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs';
 const wkid2263 = '+proj=lcc +lat_1=41.03333333333333 +lat_2=40.66666666666666 +lat_0=40.16666666666666 +lon_0=-74 +x_0=300000.0000000001 +y_0=0 +ellps=GRS80 +datum=NAD83 +to_meter=0.3048006096012192 +no_defs'
 
 let baseMapTileLayer = null
 let stormwaterTileLayer = null
-let subwayTileLayer = null
 let idaHeatMapLayer = null
 let idaPointLayer = null
 let hurricaneEvacCenterLayer = null
+
+function initSubwayBasemap(map) {
+    const blob1 = new Blob([JSON.stringify(subwayStations)], {
+        type: "application/json",
+    });
+
+    const labelClass = {
+        // autocasts as new LabelClass()
+        symbol: {
+            type: "text",
+            color: "rgb(40,40,40)",
+            haloColor: "rgba(255,255,255,0.8)",
+            haloSize: 2,
+            font: {
+                family: "Ubuntu",
+                size: 10
+            }
+        },
+        labelPlacement: "below-center",
+        labelExpressionInfo: {
+            expression: "$feature.name"
+        },
+        minScale: 20000,
+        maxScale: 0
+    };
+
+    const stations = new GeoJSONLayer({
+        url: URL.createObjectURL(blob1),
+        renderer: {
+            type: "simple",
+            symbol: {
+                type: "simple-marker",
+                color: "rgba(255,255,255, 0.8)",
+                size: 6,
+                outline: {
+                    width: 1,
+                    color: "rgba(0,0,0,0.6)",
+
+                }
+            }
+        },
+        labelingInfo: [labelClass],
+        spatialReference: {
+            wkid: 4326
+        },
+        title: "Subway Stations"
+    });
+
+    const blob2 = new Blob([JSON.stringify(subwayLines)], {
+        type: "application/json",
+    });
+
+    const lineColors = subwayLines.features.reduce((colors, { properties: prop }) => {
+        //check if color in array
+        const { color } = prop
+        if (!colors.find(item => item.value === color)) {
+            colors.push({
+                value: color,
+                symbol: {
+                    type: "simple-line",
+                    color,
+                    width: "2px",
+                    style: "solid"
+                }
+            })
+        }
+
+        return colors
+    }, [])
+
+    console.log(lineColors)
+
+    const lines = new GeoJSONLayer({
+        url: URL.createObjectURL(blob2),
+        renderer: {
+            type: "unique-value",
+            field: "color",
+            uniqueValueInfos: lineColors
+        },
+        spatialReference: {
+            wkid: 4326
+        },
+        title: "Subway Lines",
+        opacity: 0.3
+    });
+
+    map.add(lines)
+    map.add(stations)
+
+}
 
 async function initHurrShelters(map) {
     const url = 'https://corsproxy.io/?https://maps.nyc.gov/hurricane/data/center.csv'
@@ -96,10 +188,6 @@ export function initLayers(map) {
         style: stormwaterStyle,
     });
 
-    subwayTileLayer = new VectorTileLayer({
-        style: subwayStyle,
-    });
-
     const blob = new Blob([JSON.stringify(idaSr)], {
         type: "application/json",
     });
@@ -153,11 +241,14 @@ export function initLayers(map) {
     });
 
     initHurrShelters(map)
-    map.add(subwayTileLayer);
     map.add(baseMapTileLayer);
     map.add(stormwaterTileLayer);
+    initSubwayBasemap(map)
     map.add(idaHeatMapLayer)
     map.add(idaPointLayer)
+
+    idaHeatMapLayer.visible = false
+    idaPointLayer.visible = false
 
     //cover areas not in Manhattan
     const mask = new VectorTileLayer({
@@ -181,7 +272,7 @@ export function initLayers(map) {
         },
     });
 
-    map.add(mask)
+    // map.add(mask)
 
     const modStormWaterToggleLayers = [
         'Area not included in analysis:1',
@@ -197,7 +288,7 @@ export function initLayers(map) {
             name: 'Moderate Stormwater Flood with 2050 Sea Levels',
             description: 'Heavy rain events that overwhelm our stormwater management system.',
             legendElements: [
-                `<div><span class="fill" style="background-color: #73B2FF;"></span>Nuisance Flooding (greater or equal to 4 in and less than 1ft)</div>`,
+                `<div><span class="fill" style="background-color: #73B2FF;"></span>Nuisance Flooding (greater or equal to 4in and less than 1ft)</div>`,
                 `<div><span class="fill" style="background-color: #005CE6;"></span>Deep and Contiguous Flooding (1ft and greater)</div>`,
                 `<div><span class="fill" style="background-color: rgba(255, 167, 29,1);"></span>Future High Tides in 2050</div>`,
                 `<div><span class="fill" style="background-color: #CCCCCC;"></span>Area not included in analysis or National Wetlands Inventory</div>`,
@@ -219,9 +310,9 @@ export function initLayers(map) {
             name: '311 DEP Flood Service Requests (Hur. Ida: Sept 1-2 2021)',
             description: 'Complaints of Sewer Backup, Catch Basin Clogged/Flooding, and Basement Flooding. These show us issues that happen in buildings and sidewalks that the stormwater layer can miss. Zoom in to view more details.',
             legendElements: [
-                `<div><span class="circle" style="background-color: rgba(255,0,0,0.8);outline: 1px solid rgba(200,200,200,0.7);"></span>311 DEP Flood Service Requests</div>`
+                `<div><span class="circle" style="background-color: rgba(255,0,0,0.8);outline: 1px solid rgba(200,200,200,0.7);"></span>311 DEP Flood Service Requests During Ida</div>`
             ],
-            visible: true,
+            visible: false,
             src: thumb_ida_311_heatmap,
             on: (map) => {
                 idaHeatMapLayer.visible = true
