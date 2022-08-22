5348e55c7550bd77f5deaffcb3130d824d1bac97
zhik <zhikhe@gmail.com>
2022-08-22 03:46:13 +0000

    Deploying to gh-pages from @ zhik/mn-flood-maps@3c4f40301bbb658cd4f166b1d1bd1d74094942a2 🚀

--- assets/executeForIds.63ff25b1.js
+++
@@ -1,1 +0,0 @@
-import{fb as r,g1 as e,eE as c,g2 as f}from"./index.d0d680cc.js";async function m(n,t,o){const a=r(n);return e(a,c.from(t),{...o}).then(s=>s.data.count)}async function p(n,t,o){const a=r(n);return f(a,c.from(t),{...o}).then(s=>s.data.objectIds)}export{m as n,p as s};
